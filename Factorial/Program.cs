0f642cc5203ec012d7535632a542e6d50a91203e
34608 <34608@DESKTOP-IS775AF>
2021-11-19 19:50:20 +0200

    Agregar archivos de proyecto.

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, fact;
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+            fact = number;
+            for (i = number - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
